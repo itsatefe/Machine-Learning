aebc3ccc1efd7a669a0a9a8bb2b57a261c4c8efb
Atefe Rajabi <atefe.rajabi.78@gmail.com>
2024-03-15 14:51:08 +0330

    Documentation

--- HW1/Model.py
+++ HW1/Model.py
@@ -23,6 +23,7 @@ class CandidElimination:
     def __init__(self,fs):
         self.fs = fs
         
+        
     def get_all_h(self, uni):
         lens = list(map(len, uni))
         h = [np.concatenate([[uni[i][j]] * int(np.prod(lens[i + 1:])) for j in range(len(uni[i]))]).tolist() * int(np.prod(lens[:i])) for i in range(len(uni))]
@@ -36,6 +37,7 @@ class CandidElimination:
         negative = train_data[np.where(train_labels == '-')]
         self.specific = np.array([positive[0, i] if np.all(positive[:, i] == positive[0, i]) else '?' for i in range(positive.shape[1])])
 
+        # generating more general h for each h
         uni = np.array(list(zip(self.fs.h.tolist(), ['?'] * len(self.fs.h))))
         general = np.unique(self.get_all_h(uni), axis = 0)[1:-1]
         g = []
@@ -43,8 +45,44 @@ class CandidElimination:
             if np.all(self.evaluate(negative, h) == '-'):
                 g.append(h)
 
-        self.general = np.stack(g)
-        self.vs = np.concatenate([self.specific[None], self.general], axis = 0)
+        self.general = np.stack(g) if g else np.array([])
+        self.vs = np.concatenate([self.specific[None], self.general], axis = 0) if g else self.specific[None]
+        self.status = 'consistent' if np.all(self.evaluate(negative, self.specific) == '-') else 'inconsistent'
+
+    def predict(self, val_data):
+        preds = np.stack([self.evaluate(val_data, h) for h in self.vs]).T
+        final_preds = []
+        for p in preds:
+            a, b = np.unique(p, return_counts = True)
+            if len(a) == 2 and b[0] == b[1]:
+                final_preds.append(None)
+            else:
+                final_preds.append(a[b.argmax()])
+        return np.array(final_preds)
+    
+    
+    
+    
+    
+class ListThenEliminate:
+    def get_all_h(self, uni):
+        lens = list(map(len, uni))
+        h = [np.concatenate([[uni[i][j]] * int(np.prod(lens[i + 1:])) for j in range(len(uni[i]))]).tolist() * int(np.prod(lens[:i])) for i in range(len(uni))]
+        return np.array(h).T
+
+    def evaluate(self, data, h):
+        return np.where(np.all((h == data) | (h == '?'), axis = 1), '+', '-')
+
+    def fit(self, train_data, train_labels):
+        uni = [np.unique(train_data[:, i]).tolist() + ['?'] for i in range(train_data.shape[1])]
+        all_h = self.get_all_h(uni)
+        vs = []
+
+        for h in all_h:
+            if np.all(self.evaluate(train_data, h) == train_labels):
+                vs.append(h)
+
+        self.vs = np.stack(vs) if vs else np.array([])
 
     def predict(self, val_data):
         preds = np.stack([self.evaluate(val_data, h) for h in self.vs]).T
